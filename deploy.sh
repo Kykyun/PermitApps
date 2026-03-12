e18658f3f06158eb71637412305b89c7c3d5bbd5
magsysdev <magsysdev@bitbucket.org>
2026-03-13 07:38:49 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "🔄 Pulling latest code & restarting AWS server..."
-ssh -i ~/perdim/aws.pem ec2-user@47.129.247.210 "cd PermitApps && git pull && docker-compose down && docker-compose up -d --build"
+ssh -i ~/perdim/aws.pem ubuntu@47.129.247.210 "cd PermitApps && git pull && docker-compose down && docker-compose up -d --build"
 echo "✅ Done!"

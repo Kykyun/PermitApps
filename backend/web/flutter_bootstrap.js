9377a9512fff418ed7e5a5a7355213a7bd14a559
magsysdev <magsysdev@bitbucket.org>
2026-03-16 13:38:51 +0800

    logo

--- backend/web/flutter_bootstrap.js
+++ backend/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1725683104" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2024857059" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
